2644f1f1d68b40ed24e1af7dfb394d87cd95d33d
rajat-ji <mycommercialid@gmail.com>
2022-04-16 00:30:52 +0530

    i added the output statement

--- math.py
+++ math.py
@@ -1,5 +1,5 @@
 num_input1 = int(input("enter first number to multiply:"))
 num_input2 = int(input("enter Second number to multiply:"))
 mult= num_input1 * num_input2
-print(mult)
+print('your multiplication is:',mult)
 
